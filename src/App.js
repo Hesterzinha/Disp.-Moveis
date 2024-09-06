3f1288358f395173d3be91132f47c1926b2f05d5
Hesterzinha <164015806+Hesterzinha@users.noreply.github.com>
2024-09-06 12:41:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
+
+  const agora = new Date();
+  const hora = agora.getHours();
+
+  let mensagem = "u";
+
+  if (hora < 12) {
+      mensagem = "Bom dia!";
+  } else if (hora >= 12 && hora < 18) {
+      mensagem = "Boa tarde!";
+  } else {
+      mensagem = "Boa noite!";
+  }
+
+document.getElementById("mensagem").innerHTML = mensagem;
+
   return (
-    <div className="atv01">
+    <div className="atv10">
       <p id="mensagem"></p>
     </div>
   );
 }
-
-    const agora = new Date();
-    const hora = agora.getHours();
-
-    let mensagem = "";
-
-    if (hora < 12) {
-        mensagem = "Bom dia!";
-    } else if (hora >= 12 && hora < 18) {
-        mensagem = "Boa tarde!";
-    } else {
-        mensagem = "Boa noite!";
-    }
-
-    document.getElementById("mensagem").textContent = mensagem;
 
 export default App;
